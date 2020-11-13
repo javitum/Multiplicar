b4a9951826829852569a325e3850bcfd461aac39
Javier Madrazo <javitum@gmail.com>
2020-11-13 20:23:24 +0100

    Mejoras
    Guarda configuracion, 2 idiomas y aplausos

---
+++ en.ts
@@ -0,0 +1,86 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="en" sourcelanguage="es">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="Ui_multiplicar.py" line="549"/>
+        <source>Seleccionar tablas de multiplicar</source>
+        <translation>Select multiplication tables</translation>
+    </message>
+    <message>
+        <location filename="Ui_multiplicar.py" line="550"/>
+        <source>APRENDIENDO A MULTIPLICAR</source>
+        <translation>LEARNING TO MULTIPLY</translation>
+    </message>
+    <message>
+        <location filename="Ui_multiplicar.py" line="551"/>
+        <source>Seleccionar cifras de multiplicando</source>
+        <translation>Select multiplication numbers</translation>
+    </message>
+    <message>
+        <location filename="Ui_multiplicar.py" line="552"/>
+        <source>SALIR</source>
+        <translation>EXIT</translation>
+    </message>
+    <message>
+        <location filename="Ui_multiplicar.py" line="553"/>
+        <source>REINICIAR</source>
+        <translation>RESET</translation>
+    </message>
+    <message>
+        <location filename="Ui_multiplicar.py" line="554"/>
+        <source>COMPROBAR (F6)</source>
+        <translation>CHECK (F6)</translation>
+    </message>
+    <message>
+        <location filename="Ui_multiplicar.py" line="555"/>
+        <source>F6</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Ui_multiplicar.py" line="556"/>
+        <source>NUEVA (F5)</source>
+        <translation>NEW (F5)</translation>
+    </message>
+    <message>
+        <location filename="Ui_multiplicar.py" line="557"/>
+        <source>F5</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Ui_multiplicar.py" line="558"/>
+        <source>Ejercicios:</source>
+        <translation>Exercises:</translation>
+    </message>
+    <message>
+        <location filename="Ui_multiplicar.py" line="559"/>
+        <source>Bien:</source>
+        <translation>Good:</translation>
+    </message>
+    <message>
+        <location filename="Ui_multiplicar.py" line="560"/>
+        <source>Mal:</source>
+        <translation>Bad:</translation>
+    </message>
+    <message>
+        <location filename="Ui_multiplicar.py" line="561"/>
+        <source>Nota:</source>
+        <translation>Grade:</translation>
+    </message>
+    <message>
+        <location filename="Ui_multiplicar.py" line="562"/>
+        <source>Idioma</source>
+        <translation>Language</translation>
+    </message>
+    <message>
+        <location filename="Ui_multiplicar.py" line="563"/>
+        <source>Nota minima:</source>
+        <translation>Minimum grade:</translation>
+    </message>
+    <message>
+        <location filename="Ui_multiplicar.py" line="564"/>
+        <source>Ejercicios minimos:</source>
+        <translation>Minimum exercises:</translation>
+    </message>
+</context>
+</TS>
--- multiplicar.py
+++ multiplicar.py
@@ -1,4 +1,7 @@
 import sys
+from PyQt5.QtCore import pyqtSlot ,  QSettings
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5 import QtCore ,  QtWidgets , QtMultimedia
 from Ui_multiplicar import *
 from random import randint
 
@@ -6,7 +9,11 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
-        self.setWindowTitle("Multiplicar")
+        self.trans=QtCore.QTranslator(self)
+        idiomas=['Español', 'English']
+        self.comboBoxIdioma.addItems(idiomas)
+        self.settings = QSettings('javitum', 'Multiplicar')
+        if len(self.settings.childKeys()) !=0: self.cargar_configuracion()
         self.pushButtonGenerar.clicked.connect(self.Generar)
         self.pushButtonComprobar.clicked.connect(self.Comprovar)  
         self.pushButtonReiniciar.clicked.connect(self.Reiniciar)
@@ -140,10 +147,21 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.pushButtonGenerar.setEnabled(True)
         self.pushButtonComprobar.setEnabled(False)
         self.pushButtonGenerar.setFocus()
-        if ejercicios>=20 and nota>=8:
+        
+        ejercicios_min=int(self.lineEditEjercicios_min.text())
+        nota_min=int(self.lineEditNota_min.text())
+        
+        if ejercicios>=ejercicios_min and nota>=nota_min:
             self.corazon.setGeometry(1260,520,220,216)
-        else:
-            self.corazon.setGeometry(1260,520,1,1)
+            self.pushButtonGenerar.setEnabled(False)
+            self.pushButtonComprobar.setEnabled(False)
+            filename = 'Aplausos.mp3'
+            #fullpath = QtCore.QDir.current().absoluteFilePath(filename) 
+            media = QtCore.QUrl.fromLocalFile(filename)
+            content = QtMultimedia.QMediaContent(media)
+            self.player = QtMultimedia.QMediaPlayer()
+            self.player.setMedia(content)
+            self.player.play()
           
     def Reiniciar(self):
         self.textEditllevadas1.setText("")
@@ -165,12 +183,69 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.labelnota.setText("")
         self.pushButtonGenerar.setEnabled(True)
         self.pushButtonComprobar.setEnabled(False)
+        self.corazon.setGeometry(1260,520,1,1)
+        MainWindow.guardar_configuracion(self) 
                      
     def Salir(self):
+        MainWindow.guardar_configuracion(self) 
         sys.exit()
+
+    @pyqtSlot(str)
+    def on_comboBoxIdioma_currentIndexChanged(self, p0):
+        idiomas={'Español':'', 'English':'en'}
+        idioma_seleccionado=idiomas[p0]
+        if idioma_seleccionado:
+            self.trans.load(str(idioma_seleccionado))
+            QtWidgets.QApplication.instance().installTranslator(self.trans)
+        else:
+            QtWidgets.QApplication.instance().removeTranslator(self.trans)
+        self.retranslateUi(MainWindow)
+     
+    def guardar_configuracion(self):
+        self.settings.setValue('idioma', self.comboBoxIdioma.currentText())
+        self.settings.setValue('ejer_min', self.lineEditEjercicios_min.text())
+        self.settings.setValue('nota_min', self.lineEditNota_min.text())
+        self.settings.setValue('1', self.checkBox.isChecked())
+        self.settings.setValue('2', self.checkBox_2.isChecked())
+        self.settings.setValue('3', self.checkBox_3.isChecked())
+        self.settings.setValue('4', self.checkBox_4.isChecked())
+        self.settings.setValue('5', self.checkBox_5.isChecked())
+        self.settings.setValue('6', self.checkBox_6.isChecked())
+        self.settings.setValue('7', self.checkBox_7.isChecked())
+        self.settings.setValue('8', self.checkBox_8.isChecked())
+        self.settings.setValue('9', self.checkBox_9.isChecked())
+        self.settings.setValue('10', self.checkBox_10.isChecked())
+        self.settings.setValue('11', self.checkBox_11.isChecked())
+        self.settings.setValue('12', self.checkBox_12.isChecked())
+        self.settings.setValue('13', self.checkBox_13.isChecked())
+        self.settings.setValue('14', self.checkBox_14.isChecked())
+        self.settings.setValue('15', self.checkBox_15.isChecked())
         
+    def cargar_configuracion(self):
+        self.comboBoxIdioma.setCurrentText(self.settings.value('idioma'))
+        self.lineEditEjercicios_min.setText(self.settings.value('ejer_min'))
+        self.lineEditNota_min.setText(self.settings.value('nota_min'))
+        self.checkBox.setChecked(eval(self.settings.value('1').capitalize()))
+        self.checkBox_2.setChecked(eval(self.settings.value('2').capitalize()))
+        self.checkBox_3.setChecked(eval(self.settings.value('3').capitalize()))
+        self.checkBox_4.setChecked(eval(self.settings.value('4').capitalize()))
+        self.checkBox_5.setChecked(eval(self.settings.value('5').capitalize()))
+        self.checkBox_6.setChecked(eval(self.settings.value('6').capitalize()))
+        self.checkBox_7.setChecked(eval(self.settings.value('7').capitalize()))
+        self.checkBox_8.setChecked(eval(self.settings.value('8').capitalize()))
+        self.checkBox_9.setChecked(eval(self.settings.value('9').capitalize()))
+        self.checkBox_10.setChecked(eval(self.settings.value('10').capitalize()))
+        self.checkBox_11.setChecked(eval(self.settings.value('11').capitalize()))
+        self.checkBox_12.setChecked(eval(self.settings.value('12').capitalize()))
+        self.checkBox_13.setChecked(eval(self.settings.value('13').capitalize()))
+        self.checkBox_14.setChecked(eval(self.settings.value('14').capitalize()))
+        self.checkBox_15.setChecked(eval(self.settings.value('15').capitalize()))  
+
+
+
+  
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     window = MainWindow()
     window.show()
-    app.exec_()
+    sys.exit(app.exec_())
